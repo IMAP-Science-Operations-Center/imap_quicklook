1bd2c6c32aa2ecd27e18806c1734a70b8900136c
Ana Manica <ana@manica.org>
2025-08-01 13:27:47 -0600

    Swapi quicklooks (#2)
    * Writing the code to generate quicklooks for SWAPI instrument.

--- tests/test_quicklook_generator.py
+++ tests/test_quicklook_generator.py
@@ -3,6 +3,7 @@
 from plotting.quicklook_generator import (
     IdexQuicklookGenerator,
     MagQuicklookGenerator,
+    SwapiQuicklookGenerator,
     get_instrument_quicklook,
 )
 
@@ -11,6 +12,8 @@ def test_generate_quicklook_instance():
     """Test if the correct class is generated."""
     mag_file = "imap_mag_l1a_norm-magi_20251017_v001.cdf"
     idex_file = "imap_idex_l1a_sci-1week_20231018_v006.cdf"
+    swapi_file = "imap_swapi_l2_quicklook_00000000_v000.cdf"
 
     assert isinstance(get_instrument_quicklook(mag_file), MagQuicklookGenerator)
     assert isinstance(get_instrument_quicklook(idex_file), IdexQuicklookGenerator)
+    assert isinstance(get_instrument_quicklook(swapi_file), SwapiQuicklookGenerator)
